8ca97a2624df73a80081397da7eed0c7a2afc0a3
arinkal12 <evgenika4@yandex.ru>
2023-10-22 20:14:36 +0300

    werty

--- 1-type/clock/clock.h
+++ 1-type/clock/clock.h
@@ -2,6 +2,6 @@ int get_time(long long angle) {
     int chas = angle/(360*60);
     int vsp = angle % (360 * 60);
     int min = vsp / 360;
-    int sek = (vsp % 360) / 6;
-    return chas*10000+ min*100+sek; // Not implementation
+    int seki = (vsp % 360) / 6;
+    return chas*10000 + min*100+seki; // Not implementation
 }
--- 2-if_else_switch/IceCream/icecream.h
+++ 2-if_else_switch/IceCream/icecream.h
@@ -1,6 +1,6 @@
 int icecream(int x){
-    if (x<3){
-        return 0;
+    if (x < 3){
+        return 0 ;
     }
     else if ((x%3==0)||(x%5==0)|| (x==8))
     {
