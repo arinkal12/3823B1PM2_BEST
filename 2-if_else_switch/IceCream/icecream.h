eab91fc72d17229cc4bc77e8aebb6571451df86f
arinkal12 <evgenika4@yandex.ru>
2023-10-22 20:21:50 +0300

    funt

--- 2-if_else_switch/IceCream/icecream.h
+++ 2-if_else_switch/IceCream/icecream.h
@@ -2,7 +2,7 @@ int icecream(int x){
     if (x < 3){
         return 0 ;
     }
-    else if ((x%3==0)||(x%5==0)|| (x==8))
+    else if ((x%3==0)||(x % 5 == 0)|| (x==8))
     {
         return 1;
     }
