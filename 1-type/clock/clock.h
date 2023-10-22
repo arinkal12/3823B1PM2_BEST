5079dd1011bde36602db5ce1844820d9d5ddec65
arinkal12 <evgenika4@yandex.ru>
2023-10-22 20:09:33 +0300

    opjl

--- 1-type/clock/clock.h
+++ 1-type/clock/clock.h
@@ -1,3 +1,7 @@
 int get_time(long long angle) {
-    return 0; // Not implementation
+    int chas = angle/(360*60);
+    int vsp = angle % (360 * 60);
+    int min = vsp / 360;
+    int sek = (vsp % 360) / 6;
+    return chas*10000+ min*100+sek; // Not implementation
 }
