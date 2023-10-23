85df7ab084995209b45bee43b449f9c88b65ff66
arinkal12 <evgenika4@yandex.ru>
2023-10-23 14:09:52 +0300

    ungtu

--- 2-if_else_switch/IceCream/icecream.h
+++ 2-if_else_switch/IceCream/icecream.h
@@ -1,8 +1,8 @@
 int icecream(int x){
-    if (x < 3){
+    if ((x < 3) || (x==4)){
         return 0 ;
     }
-    else if ((x%3==0)||(x % 5 == 0)|| (x==8))
+    else if (((x % 3 == 0) || (x % 5 == 0)||(x%8==0)))
     {
         return 1;
     }
