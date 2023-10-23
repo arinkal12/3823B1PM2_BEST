a74a6bb8a4cea307668eae0f542fcbd1c8e696a9
arinkal12 <evgenika4@yandex.ru>
2023-10-23 14:32:27 +0300

    xdfvgb

--- 2-if_else_switch/IceCream/icecream.h
+++ 2-if_else_switch/IceCream/icecream.h
@@ -1,10 +1,7 @@
 int icecream(int x){
-    if ((x < 3) || (x==4)){
-        return 0 ;
-    }
-    else if (((x % 3 == 0) || (x % 5 == 0)||(x%8==0)))
-    {
-        return 1;
-    }
+    if (x == 8) return 1;
+    if ((x < 3)) return 0 ;
+    if (((x % 3 == 0) || (x % 5 == 0))) return 1;
+    else return 0;
     
 }
