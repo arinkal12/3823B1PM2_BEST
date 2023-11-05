5e1154090f097be79d13421dd96633e2540dea82
arinkal12 <evgenika4@yandex.ru>
2023-11-05 12:02:21 +0300

    mmmmmm

--- 3-loop/find_digit/find_digit.h
+++ 3-loop/find_digit/find_digit.h
@@ -1,7 +1,15 @@
 #include <math.h>
+#include <stdio.h>
 
 char find_digit(double number, char digit){
-    if ((digit<0)||(digit>10)) return -1;
+    char str[100];
+    sprintf(str, "%f", number); 
+    int length = strlen(str);
     
+    for (int i = 0; i < length; i++) {
+        if (str[i] == digit) {
+            return i + 1; 
+        }
     
+    }
 }
