2da16f83a6b6b1c0b3970c119cd75604e5eedc63
arinkal12 <evgenika4@yandex.ru>
2023-11-07 22:07:53 +0300

    rtDYGV

--- 4-array/det/determinant.h
+++ 4-array/det/determinant.h
@@ -1,5 +1,6 @@
 #include <math.h>
 
 int determinant(int matrix[], int size){
+    if (size == 4) return (matrix[0]*matrix[3]- matrix[2]*matrix[1]);
     return 0;
 }
--- 4-array/get_percent/calc_percent.h
+++ 4-array/get_percent/calc_percent.h
@@ -1,5 +1,24 @@
 #include <math.h>
 
 int calc_percent(int matrix[], int size){
-    return 0;
+    int max = 0;
+    int min = 0;
+    for( int i =0; i !=size; i++){
+        if( matrix[i]> max){
+            max= matrix[i];
+        }
+        if (matrix[i]<min){
+            min = matrix[i];
+        }
+
+    }
+    int r = (max-min)/2;
+    int count = 0;
+    for(int i =0; i !=size; i++){
+        if (matrix[i]>r){
+            count++;
+        }
+    }
+    
+    return (count * 100)/size;
 }
