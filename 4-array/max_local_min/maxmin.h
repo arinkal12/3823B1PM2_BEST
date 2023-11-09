8d786af6e222ee0bf717f4a07fd7ace7c80d8d8a
arinkal12 <evgenika4@yandex.ru>
2023-11-09 21:49:47 +0300

    ytghhou

--- 4-array/max_local_min/maxmin.h
+++ 4-array/max_local_min/maxmin.h
@@ -1,3 +1,20 @@
 int maxmin(int input[], int size){
-    return -1;
+    int col = 0;
+    int max=0;
+    for (int i =1; i< size;i++){
+        if ((input[i]<input[i+1]) && (input[i]<input[i-1])) {
+            if (max< input[i]) max = input[i];
+
+        }
+    if (input[size]<input[size-1]){
+        if (max <input[size] ) max = input[size];
+    }
+    if (input[0]<input[1]){
+        if (max <input[0] ) max = input[0];
+    }    
+        
+
+
+    }
+    return max;
 }
--- 4-array/more_on_left/left_more.h
+++ 4-array/more_on_left/left_more.h
@@ -1,3 +1,7 @@
 char left_more(double input[], unsigned int size){
-    return -1;
+    int col =0;
+    for (int i=1; i<size; i++){
+        if (input[i] > input[i - 1]) col++;
+    }
+    return col;
 }
