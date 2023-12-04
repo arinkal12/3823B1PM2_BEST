77d5bd1b512ab9664e2afadda77c1134c47c5d4f
arinkal12 <evgenika4@yandex.ru>
2023-12-04 21:09:41 +0300

    wehyfvujwv

--- 6-functions/one_array/generator.h
+++ 6-functions/one_array/generator.h
@@ -1,18 +1,14 @@
 #include <math.h>
 #include <stdlib.h>
-int* gen(int size, int f, int k, int m ){
-    int* arr=malloc(sizeof(int)*size);
-    arr[0]=f;
-    for (int i=1; i<size; i++){
-        arr[i]=(k-m)/(4);
-    }
-    return arr;
-}
-
 
 int* array_generator(int* real_size, int k, int m, int f){
-    *real_size=10+rand()%90;
-    int* arr=gen(*real_size, f,k,m);
+    int a = 55;
+    *real_size=a;
+    int* mass = malloc(a*sizeof(int));
+    mass[0]= f;
+    for (int i = 1; i< a; i++){
+        mass[i]=0;
+    }
     
-    return arr;
+    return mass;
 }
