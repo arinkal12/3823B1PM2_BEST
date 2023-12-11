ec91e278b60e8ffee7c55c263d22f3b324553e5a
arinkal12 <evgenika4@yandex.ru>
2023-12-11 17:44:17 +0300

    wqfvhbqiw

--- 7-recursion/calc_row_sum/sum_series_terms.h
+++ 7-recursion/calc_row_sum/sum_series_terms.h
@@ -1,16 +1,18 @@
 #include <math.h>
 #include <stdio.h>
 
-double sum_series(int n){
-    
-   // if (n<=0){
-        // return-1;
-        
-   // }
+double posl(int n){
     if (n == 0) {
         return 0;
     } else {
-        return (1.0 / n) +  sum_series(n - 1);
+        return 1.0/n + posl(n-1);
     }
+
+}
+double sum_series(int n){
+    if (n <=0) {
+        return -1;
+    }
+    else return posl(n);
     
 }
--- 7-recursion/recursion_array_sum/calc_sum_array_elements.h
+++ 7-recursion/recursion_array_sum/calc_sum_array_elements.h
@@ -1,10 +1,15 @@
 #include <math.h>
+int sum_ar(int* arr, int size){
+    if (size==0){
+        return 0;
+    }
+    else {
+        return arr[size-1] + sum_ar(arr, size - 1);
+    }
+
+}
 
 double calc_sum_elements(int* array, int array_size){
     if (array_size<=0) return -1;
-    if (array_size==0){
-        return 0;
-    }
-    else return array[array_size-1] + calc_sum_elements(array, array_size - 1);
-    
+    else return sum_ar(array, array_size);
 }
