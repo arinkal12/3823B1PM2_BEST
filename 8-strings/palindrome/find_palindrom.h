cbc059739ef5551cef725fdd8edbcdeae270df02
arinkal12 <evgenika4@yandex.ru>
2023-12-19 09:17:55 +0300

    c6tfvgbn

--- 8-strings/palindrome/find_palindrom.h
+++ 8-strings/palindrome/find_palindrom.h
@@ -2,5 +2,27 @@
 #include <string.h>
 
 int is_palindrom(char text[]){
-    return -100;
+    int len = strlen(text);
+    int j = len-1;
+    int len_now =0;
+    char now_text[];
+    for (int i =0; i<len;i++){
+        if (text[i]!= ' '){
+            now_text[j] = text[i];
+            j++;
+
+        }
+    }
+    now_text[j] = '\0';
+    int q = strlen(now_text)-1;
+    if (len ==0) return 1;
+    else{
+        for( int i =0; i<j;i++,j--){
+            if (now_text[i]!= now_text[j]){
+                return 0;
+            }
+            else return 1;
+        }
+    }
+    
 }
