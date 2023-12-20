b83698d944b0021bc0b8e08c1613dee770892a55
arinkal12 <evgenika4@yandex.ru>
2023-12-20 15:31:30 +0300

    tyiwcuqv

--- 8-strings/double_to_str/double_to_str_convert.h
+++ 8-strings/double_to_str/double_to_str_convert.h
@@ -1,6 +1,49 @@
 #include <math.h>
+#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 char* convert(double number){
-    return NULL;
+    char* str =(char*)malloc(20 * sizeof(char));
+    sprintf(str, "%f", number); 
+    return str;
 }
+
+
+
+
+
+
+    /*char* str= (char*)(malloc(sizeof(number)));
+    int cel_cast = int(number);
+    double pr = number - cel_cast;
+    double p = number - cel_cast;
+    int count = 0;
+    int integer = (int)pr; 
+    while (integer != pr) //кол-во знаокв после занятой
+    {
+        pr *= 10; 
+        count++;
+        integer = (int)pr;
+    }
+    int posl_zap = 0;
+    if (count == 1){
+        posl_zap = 10*p;
+    }
+    else if (count == 2)
+    {
+        posl_zap = 100*p;
+    }
+    else if (count == 3)
+    {
+        posl_zap = 1000*p;
+    }
+    else if (count == 4)
+    {
+        posl_zap = 10000*p;
+    }
+    for (int i = 0; i<count;i++){
+        str[i] = 
+    }
+  */  
+
--- 8-strings/palindrome/find_palindrom.h
+++ 8-strings/palindrome/find_palindrom.h
@@ -1,28 +1,37 @@
 #include <math.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 int is_palindrom(char text[]){
     int len = strlen(text);
-    int j = len-1;
+    int j = 0;
     int len_now =0;
-    char now_text[];
+
+    char* now_text=(char*)malloc(len*sizeof(char));
     for (int i =0; i<len;i++){
         if (text[i]!= ' '){
-            now_text[j] = text[i];
+            now_text[j] = (char)tolower(text[i]);
             j++;
 
         }
     }
     now_text[j] = '\0';
-    int q = strlen(now_text)-1;
+    j--;
     if (len ==0) return 1;
     else{
-        for( int i =0; i<j;i++,j--){
+        for( int i =0; i<(j/2);i++,j--){
             if (now_text[i]!= now_text[j]){
+                free(now_text);
                 return 0;
             }
-            else return 1;
+            else{
+                free(now_text);
+                return 1;
+            } 
         }
     }
     
+    
 }
+
